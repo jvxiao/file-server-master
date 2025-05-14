a1778c6a7246803d2c72b584246e2ee528a8a1ea
jvxiao <2805431021@qq.com>
2025-05-14 22:02:30 +0800

    feat:  get server host

---
+++ backend/src/services/systemService.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const os = require('os');
+
+
+const getServerHost = () => {
+  const interfaces = os.networkInterfaces();
+  console.log(process.env)
+  console.log('networkInterfaces: ',interfaces);
+  for (const name of Object.keys(interfaces)) {
+    for (const iface of interfaces[name] || []) {
+      if (iface.family === 'IPv4' && !iface.internal) {
+        console.info('获取局域网 IP：', iface.address)
+        return iface.address
+      }
+    }
+  }
+  console.warn('无法获取局域网 IP，使用默认 IP: 127.0.0.1')
+  return '127.0.0.1'
+}
+
+module.exports = {
+  getServerHost
+}
